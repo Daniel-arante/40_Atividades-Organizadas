fdbc9e265052654d19af06adf8bd5079807b8785
Daniel Arantes <daniellopes6707@gmail.com>
2025-06-16 20:58:38 -0300

    Update Program.cs

--- Exercicio25/Program.cs
+++ Exercicio25/Program.cs
@@ -1,11 +1,11 @@
 ﻿decimal volume, PI = 3.1416m, raio, altura;
 
-Console.WriteLine("Digite o raio da base da caixa d'água (em metros): ");
+Console.WriteLine("Digite o raio da base da caixa d'água: ");
 raio = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Digite a altura da caixa d'água (em metros): ");
+Console.WriteLine("Digite a altura da caixa d'água: ");
 altura = Convert.ToDecimal(Console.ReadLine());
 
 volume = PI * raio * raio * altura;
 
-Console.WriteLine("O volume da caixa d'água é:" + volume + "metros cúbicos.");
+Console.WriteLine("O volume da caixa d'água é:" + volume + " metros cúbicos.");
